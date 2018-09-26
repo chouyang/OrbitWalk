805f690b4736eb15d75c388f62ecedebeb6956e7
kamdude <thekamdude@gmail.com>
2018-09-26 15:31:04 +0800

    Initial Commit

---
+++ OrbitWalkTests/OrbitWalkTests.swift
@@ -0,0 +1,34 @@
+//
+//  OrbitWalkTests.swift
+//  OrbitWalkTests
+//
+//  Created by Eric Young on 9/26/18.
+//  Copyright © 2018 Eric Young. All rights reserved.
+//
+
+import XCTest
+@testable import OrbitWalk
+
+class OrbitWalkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
